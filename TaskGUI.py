01ea61df715dd97dc5a20c788b576ff970ba7942
Lincon Woodruff <lp.woodruff@outlook.com>
2024-05-23 07:51:38 -0700

    Functionality for buttons

--- TaskGUI.py
+++ TaskGUI.py
@@ -65,7 +65,8 @@ class Taskapp:
 
         self.BT_remove_task = tk.Button(
             self.controls_frame,
-            text="Remove Task")
+            text="Remove Task",
+            command=self.remove_task)
         self.BT_remove_task.pack(side=tk.LEFT)
 
         self.BT_quit = tk.Button(
@@ -100,7 +101,7 @@ class Taskapp:
         self.date_entry = tk.Entry(self.creation_frame)
         self.date_entry.pack(anchor=tk.CENTER)
 
-        self.BT_submit = tk.Button(self.creation_frame, text="Submit")
+        self.BT_submit = tk.Button(self.creation_frame, text="Submit", command=self.submit)
         self.BT_submit.pack(anchor=tk.CENTER)
         
         self.BT_cancel = tk.Button(self.creation_frame, text="Cancel", command=self.cancel_button)
@@ -112,8 +113,10 @@ class Taskapp:
 
         self.task_listbox = tk.Listbox(self.task_frame, bg="White")
         self.task_listbox.pack(fill=tk.BOTH)
-
+        
+        self.load_tasks()
         self.update_time_date()
+        
 
     def update_time_date(self):
         current_time = datetime.now().strftime("%H:%M:%S")
@@ -126,6 +129,36 @@ class Taskapp:
     def create_task(self):
         self.creation_frame.pack(fill=tk.BOTH, anchor=tk.CENTER)
 
+    def submit(self):
+        task_num = len(self.task_list) + 1
+        task_description = self.description_entry.get("1.0", "end-1c") 
+        task_date = self.date_entry.get() 
+        self.task_list.append(f"Task {task_num}: {task_description}, {task_date}")
+        with open("tasks.txt", 'a') as file:
+            file.write(self.task_list[task_num - 1] + "\n")
+        self.load_tasks()
+
     def cancel_button(self):
         self.creation_frame.pack_forget()
+
+    def load_tasks(self):
+        self.task_list.clear()
+        self.task_listbox.delete(0, tk.END) 
+        with open("tasks.txt", 'r') as file:
+            for line in file:
+                self.task_list.append(line.strip())
+            for item in self.task_list:
+                self.task_listbox.insert(tk.END, item)
         
+    def remove_task(self):
+        selected_index = self.task_listbox.curselection()
+        if selected_index:
+            task_to_remove = self.task_list[selected_index[0]]
+        
+            self.task_list.remove(task_to_remove)
+        
+            self.task_listbox.delete(selected_index)
+
+            with open("tasks.txt", 'w') as file:
+                for task in self.task_list:
+                    file.write(task + "\n")
--- main.py
+++ main.py
@@ -4,15 +4,7 @@ import tkinter as tk
 root = tk.Tk()
 
 app = Taskapp(root)
-
-
-with open("tasks.txt", 'r') as file:
-    for line in file:
-        app.task_list.append(line.strip())
-    
-for item in app.task_list:
-    app.task_listbox.insert(tk.END, item)
-        
+   
 root.mainloop()
 
     
