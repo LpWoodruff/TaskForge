44fa52f4e430380000a10cddbc5b7b256b99c364
Lincon Woodruff <lp.woodruff@outlook.com>
2024-05-22 23:13:45 -0700

    Added some visual Aesthetics

--- TaskGUI.py
+++ TaskGUI.py
@@ -9,39 +9,59 @@ class Taskapp:
         # APP SPECIFIC INFO
         self.master = master
         master.title("TaskForge")
+        master.configure(bg="Grey")
         self.task_list = []
+        master.geometry("600x500")
 
         #Title of the application
-        self.LB_title = tk.Label(master, text = "TaskForge", font=("Helvetica", 20))
+        self.main_frame = tk.Frame(master, background="Red")
+        self.LB_title = tk.Label(self.main_frame, text = "TaskForge", font=("Impact", 36), background="Red")
         self.LB_title.pack()
-
-        self.LB_description = tk.Label(master, text="What would you like to do?:")
-        self.LB_description.pack()
-
-        #Updates the label with crurent time constantly
-        self.LB_time = tk.Label(master, text = '')
+        self.LB_time2 = tk.Label(self.main_frame, text = '', background="Red", font=("Impact", 20))
+        self.LB_time = tk.Label(self.main_frame, text = '', background="Red", font=("Impact", 20))
         self.LB_time.pack()
+        self.LB_time2.pack()
+        #Updates the label with crurent time constantly
         
-       
-        # Application functionality using buttons
-        self.BT_new_task = tk.Button(master, text="New Task", command= self.create_tast)
-        self.BT_new_task.pack()
 
-        self.BT_close_tasks = tk.Button(master, text="Close Task Menu", command = self.close_task)
+        self.main_frame.pack(fill=tk.X)
         
-      
-        # List for tasks
-        self.task_listbox = tk.Listbox(master, width=100, height=20, font=("Helvetica", 20))
-        self.BT_view_tasks = tk.Button(master, text="View Tasks", command= self.view_tasks)
+        #The main menu Frame
+        self.menu_frame = tk.Frame(master, background="DarkGrey")
+        ## its elements
+        self.LB_description = tk.Label(self.menu_frame, text="What would you like to do?", font=("Helvetica", 20), background="DarkGrey")
+        self.LB_description.pack()
+        self.BT_new_task = tk.Button(self.menu_frame, text="New Task", command= self.create_task)
+        self.BT_view_tasks = tk.Button(self.menu_frame, text="View Tasks", command= self.view_tasks)
+        self.BT_new_task.pack()
         self.BT_view_tasks.pack()
 
-        self.LB_task_num = tk.Label(master, text = ' ')
-        self.task_description = tk.Entry(master)
-        self.BT_submit = tk.Button(master, text="Submit")
-        self.BT_cancel = tk.Button(master, text="Cancel", command=self.cancel_new)
+        #The View Tasks Frame
+        self.view_task_frame = tk.Frame(master, background="DarkGrey")
+        ## its elements
+        self.BT_close_tasks = tk.Button(self.view_task_frame, text="Close Task Menu", command = self.close_task)
+        self.task_listbox = tk.Listbox(self.view_task_frame, width=100, height=20, font=("Helvetica", 20), background="Red")
+        self.BT_close_tasks.pack()
+        self.task_listbox.pack()
+    
+       
+        # The New Tasks frame
+        self.new_task_frame = tk.Frame(master, background="DarkGrey")
+        ## its elements
+        self.LB_task_num = tk.Label(self.new_task_frame, text = ' ', font=("Helvetica", 20), background="Red")
+        self.LB_task_description = tk.Label(self.new_task_frame, text = 'Description:', font=("Helvetica", 20), background="DarkGrey")
+        self.task_description = tk.Text(self.new_task_frame, width=100, height=5)
+        self.BT_submit = tk.Button(self.new_task_frame, text="Submit", width=100, background="Red")
+        self.BT_cancel = tk.Button(self.new_task_frame, text="Cancel", command=self.cancel_new, width=100, background="Red")
+        
+        
+        self.LB_task_num.pack()
+        self.LB_task_description.pack()
+        self.task_description.pack()
+        self.BT_submit.pack()
+        self.BT_cancel.pack()
 
-
-
+        self.menu_frame.pack(fill=tk.BOTH)
         self.initialize_tasklist()
         self.update_time()
 
@@ -63,22 +83,20 @@ class Taskapp:
 
     # This updates time for the application
     def update_time(self):
-        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.LB_time.config(text=current_time)
+        current_date = datetime.datetime.now().strftime("%m-%d-%Y")
+        current_time = datetime.datetime.now().strftime("%I:%M:%S %p")
+        self.LB_time.config(text=current_date)
+        self.LB_time2.config(text=current_time)
         self.master.after(1000, self.update_time)
 
 
-    def create_tast(self):
+    def create_task(self):
         task_num = len(self.task_list) + 1
         self.LB_task_num.config(text=f"Task Number: {task_num}")
         
 
-        self.BT_view_tasks.pack_forget()
-        self.BT_new_task.pack_forget()
-        self.LB_task_num.pack()
-        self.task_description.pack()
-        self.BT_submit.pack()
-        self.BT_cancel.pack()
+        self.new_task_frame.pack(fill=tk.BOTH)
+        self.menu_frame.pack_forget()
 
 
     # This allows the button to view each task appended.
@@ -86,25 +104,15 @@ class Taskapp:
         self.task_listbox.delete(0, tk.END)
         for tasks in self.task_list:
             self.task_listbox.insert(tk.END, tasks)
-        self.BT_view_tasks.pack_forget()
-        self.BT_new_task.pack_forget()
-        self.BT_close_tasks.pack()
-        self.task_listbox.pack()
-        
+        self.view_task_frame.pack()
+        self.menu_frame.pack_forget()
 
     # Closes task menu
     def close_task(self):
-        self.BT_new_task.pack()
-        self.BT_view_tasks.pack()
-        self.BT_close_tasks.pack_forget()
-        self.task_listbox.pack_forget()
-        self.task_listbox.delete(0, tk.END)
+        self.view_task_frame.pack_forget()
+        self.menu_frame.pack(fill=tk.BOTH)
 
     def cancel_new(self):
-        self.LB_task_num.pack_forget()
-        self.task_description.pack_forget()
-        self.BT_submit.pack_forget()
-        self.BT_cancel.pack_forget()
-        self.BT_new_task.pack()
-        self.BT_view_tasks.pack()
+       self.new_task_frame.pack_forget()
+       self.menu_frame.pack(fill=tk.BOTH)
         
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from TaskGUI import Taskapp
 import tkinter as tk
  
 root = tk.Tk()
-root.geometry("900x1000")
 
 app = Taskapp(root)
      
